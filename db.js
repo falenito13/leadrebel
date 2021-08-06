8c3545778888b7a8caea0ab222c46c908e4d5a9f
falenito13 <56035351+falenito13@users.noreply.github.com>
2021-08-06 19:10:10 +0400

    ready for testing

--- db.js
+++ db.js
@@ -1,6 +1,3 @@
-/**
- * Database connection
- */
 import sqlite3 from 'sqlite3';
 var verbose = sqlite3.verbose();
 var db = new verbose.Database('./db/leadrebel.db', (err) => {
@@ -12,45 +9,79 @@ var db = new verbose.Database('./db/leadrebel.db', (err) => {
      */
     console.log('Connected to the leadrebel database.');
 });
+
 export function createTable() {
     /**
      * Create tables
      */
     db.serialize(() => {
-
         db.run(`CREATE TABLE IF NOT EXISTS Companies
                 (
-                    id INTEGER PRIMARY KEY,
-                    name VARCHAR NULLABLE,
-                    country VARCHAR NULLABLE,
-                    address VARCHAR NULLABLE,
-                    website VARCHAR NULLABLE,
-                    description VARCHAR NULLABLE,
-                    phone VARCHAR NULLABLE,
-                    head_count VARCHAR NULLABLE,
-                    established_year VARCHAR NULLABLE,
-                    sales_staff VARCHAR  NULLABLE,
-                    export_sales VARCHAR NULLABLE,
-                    categories VARCHAR NULLABLE,
-                    vat_code VARCHAR NULLABLE,
-                    sales_turnover VARCHAR NULLABLE,
-                    keywords  VARCHAR NULLABLE
+                    id
+                    INTEGER
+                    PRIMARY
+                    KEY,
+                    name
+                    VARCHAR
+                    NULLABLE
+                    UNIQUE,
+                    country
+                    VARCHAR
+                    NULLABLE,
+                    address
+                    VARCHAR
+                    NULLABLE,
+                    website
+                    VARCHAR
+                    NULLABLE,
+                    description
+                    VARCHAR
+                    NULLABLE,
+                    phone
+                    VARCHAR
+                    NULLABLE,
+                    head_count
+                    VARCHAR
+                    NULLABLE,
+                    established_year
+                    VARCHAR
+                    NULLABLE,
+                    sales_staff
+                    VARCHAR
+                    NULLABLE,
+                    export_sales
+                    VARCHAR
+                    NULLABLE,
+                    categories
+                    VARCHAR
+                    NULLABLE,
+                    vat_code
+                    VARCHAR
+                    NULLABLE,
+                    sales_turnover
+                    VARCHAR
+                    NULLABLE,
+                    keywords
+                    VARCHAR
+                    NULLABLE
                 )`);
     });
 }
-    export function insertTable(name,country,website,categories,address,vat,head_count,sales_staff,sales_turnover,phone_number,description,established_year,export_sales,keywordsArray){
-        db.serialize(() => {
-            db.run(`INSERT INTO Companies (name,country,address,website,description,phone,head_count,established_year,sales_staff,export_sales,
-            categories,vat_code,sales_turnover,keywords)` + ` values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`,[name,country,address,website,description,phone_number,head_count,
-            established_year,sales_staff,export_sales,categories,vat,sales_turnover,keywordsArray],function(err){
 
-                if(err){
-                    console.log(err.message);
-                }
+export async function insertTable(name, country, website, categories, address, vat, head_count, sales_staff, sales_turnover, phone_number, description, established_year, export_sales, keywordsArray) {
+    await db.serialize(async () => {
+        await db.run(`INSERT INTO Companies (name, country, address, website, description, phone, head_count,
+                                             established_year, sales_staff, export_sales,
+                                             categories, vat_code, sales_turnover,
+                                             keywords)` + ` values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, [name, country, address, website, description, phone_number, head_count,
+            established_year, sales_staff, export_sales, categories, vat, sales_turnover, keywordsArray], function (err) {
 
-                    console.log('success');
-            });
+            if (err) {
+                console.log(err.message);
+            }
+            console.log(name);
         });
-    }
+    });
+}
 
 
--- index.js
+++ index.js
@@ -6,127 +6,192 @@ import got from 'got';
 const app = express();
 var vgmUrl = 'https://www.europages.co.uk/business-directory-europe.html';
 createTable();
-var data = async () => {
-    return await got(vgmUrl,{decompress: false}).then((response) => {
-        const $ = cheerio.load(response.body);
-        let categoryLinks = $('.sectors-item__title a');
-        let linkArray = [];
-        for (let i = 0; i < categoryLinks.length; i++) {
-            /**
-             * Array of category links
-             */
-            linkArray.push($(categoryLinks)[i].attribs.href);
-        }
-        return linkArray;
-    })
 
+var data = async () => {
+    /**
+     * This method returns links of a sectors.
+     */
+    try {
+        return await got(vgmUrl, {decompress: false}).then((response) => {
+            const $ = cheerio.load(response.body);
+            let categoryLinks = $('.sectors-item__title a');
+            let linkArray = [];
+            for (let i = 0; i < categoryLinks.length; i++) {
+                /**
+                 * Array of category links
+                 */
+                linkArray.push($(categoryLinks)[i].attribs.href);
+            }
+            return linkArray;
+        })
+    } catch (err) {
+        console.log(err.message, 'data');
+    }
 };
 
 async function gotTemplate(url) {
-    return await got(url).then(r => r.body)
-        .then(cheerio.load)
+    /**
+     * This method loads html
+     */
+    try {
+        return await got(url).then(r => r.body)
+            .then(cheerio.load)
+    } catch (err) {
+        console.log(err.message, 'gotTemplate');
+    }
 }
 
 async function redirectTo() {
-    return await data().then((ans) => {
-        ans.forEach((el, j) => {
-            return gotTemplate(el)
-                .then(resp => {
-                    let sectors = resp('.domain-columns ul li input');
-                    var arr = [];
-                    for (let i = 0; i < sectors.length; i++) {
-                        arr.push(`ih=${sectors[i].attribs.id}&`);
-                    }
-                    companiesUrl(arr);
-                })
+    /**
+     * This method gives sector ids of all category.
+     */
+    try {
+        return await data().then((ans) => {
+            ans.forEach((el, j) => {
+                return gotTemplate(el)
+                    .then(resp => {
+                        let sectors = resp('.domain-columns ul li input');
+                        var arr = [];
+                        for (let i = 0; i < sectors.length; i++) {
+                            arr.push(`ih=${sectors[i].attribs.id}&`);
+                        }
+                        companiesUrl(arr);
+                    })
+            });
         });
-    });
+    } catch (err) {
+        console.log(err.message, 'redirectTo');
+    }
 }
 
 function companiesUrl(ans) {
-    let url = 'https://www.europages.co.uk/companies/results.html?';
-    ans.forEach((el) => {
-        url += el;
-    });
-    parseHtml(url);
+    /**
+     * This method gives sectors url
+     */
+    try {
+        let url = 'https://www.europages.co.uk/companies/results.html?';
+        ans.forEach((el) => {
+            url += el;
+        });
+        parseHtml(url);
+    } catch (err) {
+        console.log(err.message, 'companiesUrl');
+    }
 }
 
 function check(test) {
-    return typeof(test) === undefined ? test = 'null' : test;
+    /**
+     * it checks if variable is undefined
+     */
+    try {
+        return typeof (test) === undefined ? test = 'null' : test;
+    } catch (err) {
+        console.log(err.message, 'check');
+    }
 }
 
 function parseHtml(url) {
-    return gotTemplate(url,{decompress: false})
-        .then(resp => {
-            let pageArray = [];
-            let selector = resp('.pagination-nav a');
-            for (let i = 0; i < resp('.pagination-nav a').length; i++) {
-                if (!selector[i].children[0].data.includes('\n')) {
-                    pageArray.push(selector[i].children[0].data);
+    /**
+     * It takes each sector url and send all page url of each sector to getRequests method
+     */
+    try {
+        return gotTemplate(url, {decompress: false})
+            .then(resp => {
+                let pageArray = [];
+                let selector = resp('.pagination-nav a');
+                for (let i = 0; i < resp('.pagination-nav a').length; i++) {
+                    if (!selector[i].children[0].data.includes('\n')) {
+                        pageArray.push(selector[i].children[0].data);
+                    }
                 }
-            }
-            const maxPage = Math.max(...pageArray);
-            return maxPage;
-        }).then(async (max) => {
-            String.prototype.splice = function (start, delCount, newSubStr) {
-                return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
-            };
-            var regex = /companies/g;
-            if (regex.test(url) == true) {
+                const maxPage = Math.max(...pageArray);
+                return maxPage;
+            }).then(async (max) => {
+                String.prototype.splice = function (start, delCount, newSubStr) {
+                    return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+                };
+                var regex = /companies/g;
+                if (regex.test(url) == true) {
 
-                var index = regex.lastIndex + 1;
-                let requestArray = [];
-                for (let i = 1; i < max + 1; i++) {
-                    var newUrl = url.splice(index, 0, `pg-${i}/`);
+                    var index = regex.lastIndex + 1;
+                    let requestArray = [];
+                    for (let i = 1; i < max + 1; i++) {
+                        var newUrl = url.splice(index, 0, `pg-${i}/`);
                         await getRequests(newUrl);
                     }
                 }
-        })
-
-
+            })
+    } catch (err) {
+        console.log(err.message, 'parseHtml');
+    }
 }
 
-async function getRequests(request){
-        return await gotTemplate(request,{decompress: false})
+async function getRequests(request) {
+    /**
+     * It send only 5 request at once simultaneously
+     */
+    try {
+        return await gotTemplate(request, {decompress: false})
             .then(async resp => {
                 let companyLinks = resp('.company-info a');
                 let companiesArray = [];
                 for (let i = 0; i < companyLinks.length; i++) {
                     companiesArray.push(companyLinks[i].attribs.href);
-                    if(companiesArray.length>=5){
+                    if (companiesArray.length >= 5) {
                         await sendRequest(companiesArray);
                         companiesArray = [];
                     }
                 }
             });
-
+    } catch (err) {
+        console.log(err.message, 'getRequests');
+    }
 }
 
-async function sendRequest(url)
-{
-    for (let x of url) {
-      await   gotTemplate(x,{decompress: false})
+async function sendRequest(url) {
+    /**
+     * This method extract data from appropriate url and insert it into companies table
+     */
+    try {
+        for (let x of url) {
+            await gotTemplate(x, {decompress: false})
                 .then(ans => {
-                    var name = check(ans('.company-baseline h1 span').text());
-                    var country = check(ans('.company-country span')[1].children[0].data);
-                    var address = check(ans('dd[itemprop="addressLocality"]').text());
-                    var vat = check(ans('dd span[itemprop="vatID"]').text());
-                    var categories = check(ans('.js-breadcrumb-back-heading a')[0].attribs.title);
-                    var head_count = check(ans('.data-list li .icon-key-people').text());
-                    var sales_staff = check(ans('.data-list li .icon-key-sales').text());
-                    var sales_turnover = check(ans('.data-list li .icon-key-ca').text());
-                    var phone_number = check(ans('.js-num-tel').text().replace(/\s+/g, ''));
-                    var website = check(ans('.page__layout-sidebar--container-desktop .page-action[itemprop="url"]')[0].attribs.title);
-                    var export_sales = check(ans('.data-list li .icon-key-export').text());
-                    var keyword_tags = check(ans('.keyword-tag li[itemprop="itemListElement"]'));
-                    var description = check(ans('.company-description').text());
-                    var established_year = check(ans('.organisation-list li .data--big').text());
-                    var keywordsArray = [];
-                    for (let j = 0; j < keyword_tags.length; j++) {
-                        keywordsArray.push(keyword_tags[j].children[0].data);
+                        var name = check(ans('.company-baseline h1 span').text());
+                        if (ans('.company-country span').length > 1) {
+                            var country = check(ans('.company-country span')[1].children[0].data);
+                        } else {
+                            var country = 'null';
+                        }
+                        var address = check(ans('dd[itemprop="addressLocality"]').text());
+                        var vat = check(ans('dd span[itemprop="vatID"]').text());
+                        if (ans('.js-breadcrumb-back-heading a').length > 0) {
+                            var categories = check(ans('.js-breadcrumb-back-heading a')[0].attribs.title);
+                        } else {
+                            var categories = 'null';
+                        }
+                        var head_count = check(ans('.data-list li .icon-key-people').text());
+                        var sales_staff = check(ans('.data-list li .icon-key-sales').text());
+                        var sales_turnover = check(ans('.data-list li .icon-key-ca').text());
+                        var phone_number = check(ans('.js-num-tel').text().replace(/\s+/g, ''));
+                        if (ans('.page__layout-sidebar--container-desktop .page-action[itemprop="url"]').length > 0) {
+                            var website = ans('.page__layout-sidebar--container-desktop .page-action[itemprop="url"]')[0].attribs.title;
+                        } else {
+                            var website = 'null';
+                        }
+                        var export_sales = check(ans('.data-list li .icon-key-export').text());
+                        var keyword_tags = check(ans('.keyword-tag li[itemprop="itemListElement"]'));
+                        var description = check(ans('.company-description').text());
+                        var established_year = check(ans('.organisation-list li .data--big').text());
+                        var keywordsArray = [];
+                        for (let j = 0; j < keyword_tags.length; j++) {
+                            keywordsArray.push(keyword_tags[j].children[0].data);
+                        }
+                        insertTable(name, country, website, categories, address, vat, head_count, sales_staff, sales_turnover, phone_number, description, established_year, export_sales, keywordsArray);
                     }
-                    insertTable(name, country,website,categories,address, vat, head_count, sales_staff, sales_turnover, phone_number, description, established_year, export_sales, keywordsArray);
-                });
+                );
+        }
+    } catch (err) {
+        console.log(err.message, 'sendRequest');
     }
 }
 
